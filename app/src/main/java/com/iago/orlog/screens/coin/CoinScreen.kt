94ac0d4b23a08b77c47759460ff568a4d3d1cc59
Iago Mendes <iago_mendesxx@hotmail.com>
2023-01-25 18:58:35 -0300

    coin result ux

--- app/src/main/java/com/iago/orlog/screens/coin/CoinScreen.kt
+++ app/src/main/java/com/iago/orlog/screens/coin/CoinScreen.kt
@@ -1,6 +1,9 @@
 package com.iago.orlog.screens.coin
 
 import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -25,6 +28,7 @@ import kotlinx.coroutines.launch
 fun CoinScreen(navController: NavHostController, viewModel: ViewModelOrlog) {
 
     val coroutineScope = rememberCoroutineScope()
+    var played = remember { mutableStateOf(false) }
     var currentRotation = remember { mutableStateOf(0f) }
     var decision = remember { mutableStateOf<COIN>(COIN.UNDEFINED) }
     var coinResult = remember { mutableStateOf<COIN>(COIN.UNDEFINED) }
@@ -38,27 +42,63 @@ fun CoinScreen(navController: NavHostController, viewModel: ViewModelOrlog) {
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Header()
+        Header(decision.value, coinResult.value, played.value)
         Coin(rotation, coinResult.value, viewModel, 125.dp) {
             if (decision.value != COIN.UNDEFINED && coinResult.value == COIN.UNDEFINED)
                 animation(rotation, currentRotation, coinResult, coroutineScope) {
-                    decideTurns(decision.value, coinResult.value, viewModel, navController)
+                    decideTurns(decision.value, coinResult.value, viewModel, played)
                 }
         }
-        ButtonsHeadNTail(decision)
+        if (played.value)
+            ButtonChooseGodFavors(navController)
+         else
+            ButtonsHeadNTail(decision)
     }
 }
 
 @Composable
-fun Header() {
+fun ButtonChooseGodFavors(navController: NavHostController) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(.9f)
+            .padding(horizontal = 10.dp)
+            .background(
+                shape = MaterialTheme.shapes.large,
+                color = MaterialTheme.colors.onBackground,
+            )
+            .border(
+                width = 1.dp,
+                shape = MaterialTheme.shapes.large,
+                color = MaterialTheme.colors.secondary,
+            )
+            .padding(vertical = 10.dp, horizontal = 20.dp)
+            .clickable { navController.navigate(Screens.GodsScreen.name) },
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            style = MaterialTheme.typography.body2,
+            color = MaterialTheme.colors.secondary,
+            text = stringResource(R.string.select_gods_favors),
+        )
+    }
+}
+
+@Composable
+fun Header(decision: COIN, coinResult: COIN, played: Boolean) {
+
+    val winOrLose = if (decision === coinResult) R.string.won else R.string.lost
+    val player = if (decision === coinResult) R.string.you_are_player1 else R.string.you_are_player2
+
+
     Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.Start) {
         Text(
-            text = stringResource(R.string.coin),
+            text = stringResource(if (played) winOrLose else R.string.coin),
             style = MaterialTheme.typography.h1,
             color = MaterialTheme.colors.primary,
         )
         Text(
-            text = stringResource(R.string.coin_desc),
+            text = stringResource(if (played) player else R.string.coin_desc),
             style = MaterialTheme.typography.h2,
             color = MaterialTheme.colors.secondary,
         )
@@ -87,7 +127,7 @@ fun decideTurns(
     decision: COIN,
     coinResult: COIN,
     viewModel: ViewModelOrlog,
-    navController: NavHostController
+    played: MutableState<Boolean>
 ) {
     val player1Turn = if (decision === coinResult) decision else getOppositeCoin(decision)
     val player2Turn = getOppositeCoin(player1Turn)
@@ -99,7 +139,7 @@ fun decideTurns(
     if (viewModel.mode.value === MODES.ONE_PLAYER)
         viewModel.iaTurn.value = getOppositeCoin(decision)
 
-    navController.navigate(Screens.GodsScreen.name)
+    played.value = true
 }
 
 fun getOppositeCoin(coin: COIN): COIN {
